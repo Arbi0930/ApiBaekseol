e62b3a2aae081fbcb5e2cb467904c6d31f015663
Arbi0930 <mbsavage0930@gmail.com>
2022-12-05 04:36:25 +0800

    Api

---
+++ lib/users_Page.dart
@@ -0,0 +1,308 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:lapp/Home_page.dart';
+import 'package:http/http.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:lapp/api%20&%20bloc/api_controller.dart';
+
+class UserPage extends StatefulWidget {
+  final String? userName;
+  UserPage({
+    Key? key,
+    this.userName,
+  }) : super(key: key);
+
+  @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  final _checkButtonkey = GlobalKey<FormState>();
+  final _UserFormKey = GlobalKey<FormState>();
+  _checkButton() {
+    if (_checkButtonkey.currentState!.validate()) {
+      var isValid = ApiManager.checkUserValidate();
+      if (isValid != false) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Hereglegch bvrtgeltei bna "),
+          ),
+        );
+      }
+    }
+  }
+
+  void _registerButton() {
+    if (_UserFormKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Амжилттай Бүртгэлээ"),
+        ),
+      );
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ),
+      );
+    }
+    ;
+  }
+
+  void _logOutButton() {}
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 78.0),
+                child: Text(
+                  "${widget.userName}",
+                  style: TextStyle(
+                    backgroundColor: Color.fromARGB(255, 253, 255, 217),
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        leading: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll<Color>(
+              Color.fromARGB(255, 253, 255, 217),
+            ),
+            elevation: MaterialStatePropertyAll<double>(0),
+          ),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomePage()));
+          },
+          child: FaIcon(
+            FontAwesomeIcons.arrowLeft,
+            color: Colors.black,
+          ),
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        toolbarHeight: 90,
+        backgroundColor: Color.fromARGB(255, 253, 255, 217),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/back1.jpg'),
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: Form(
+              key: _UserFormKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Хэрэглэгч бүртгэх",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Form(
+                          key: _checkButtonkey,
+                          child: SizedBox(
+                            width: screenWidth / 1.7,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Container(
+                                padding: EdgeInsets.only(left: 16, right: 16),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(color: Colors.white),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: TextFormField(
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return "Утасны дугаар хоосон байна";
+                                    }
+                                    return null;
+                                  },
+                                  decoration: InputDecoration(
+                                    label: Text("Утасны дугаар"),
+                                    labelStyle:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  keyboardType: TextInputType.number,
+                                  maxLength: 8,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(1.0),
+                          child: SizedBox(
+                            width: 145,
+                            height: 77,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.redAccent),
+                                  color: Color(0xffB0ECFF),
+                                  borderRadius: BorderRadius.circular(20)),
+                              child: Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: ElevatedButton(
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        Color(0xffB0ECFF)),
+                                    foregroundColor:
+                                        MaterialStateProperty.all(Colors.black),
+                                    elevation:
+                                        MaterialStateProperty.all<double>(0),
+                                  ),
+                                  onPressed: _checkButton,
+                                  child: Text(
+                                    "Шалгах",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        padding: EdgeInsets.only(left: 16, right: 16),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Хэрэглэгчийн нэр хоосон байна";
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                              label: Text("Хэрэглэгчийн нэр"),
+                              labelStyle: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              )),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        padding: EdgeInsets.only(left: 16, right: 16),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Хэрэглэгчийн мэйл хаяг хоосон байна";
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                              label: Text("Хэрэглэгчийн мэйл хаяг"),
+                              labelStyle: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              )),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        padding: EdgeInsets.only(left: 16, right: 16),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Хэрэглэгчийн гэрийн хаяг хоосон байна";
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            label: Text("Хэрэглэгчийн гэрийн хаяг"),
+                            labelStyle: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        padding: EdgeInsets.only(
+                            left: 60, right: 60, top: 10, bottom: 10),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            elevation: MaterialStateProperty.all<double>(0),
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.white),
+                            foregroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                          ),
+                          onPressed: _registerButton,
+                          child: Text(
+                            "Бүртгэх",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
